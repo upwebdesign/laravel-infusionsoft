cd1de66b7c3054013dd2094cccaf05955e80fbf0
Jesse Vista <jvista@upwebdesign.com>
2021-10-19 17:21:35 -0400

    Add ability to connect to multiple Infusionsoft accounts

--- src/Infusionsoft.php
+++ src/Infusionsoft.php
@@ -10,11 +10,7 @@ namespace Upwebdesign\Infusionsoft;
  * @package Upwebdesign\Infusionsoft
  */
 
-use Storage;
-use Infusionsoft\Token;
 use Infusionsoft\Infusionsoft as Inf;
-use Illuminate\Support\Facades\Request;
-use Infusionsoft\InfusionsoftException;
 
 class Infusionsoft extends Inf
 {
@@ -33,47 +29,93 @@ class Infusionsoft extends Inf
     protected $authorization_code;
 
     /**
+     * Cache store
+     *
+     * @var string
+     */
+    protected $store;
+
+    /**
      * Create a new confide instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($account = false)
     {
-        $this->setTokenName(config('infusionsoft.token_name'));
-        $this->setClientId(config('infusionsoft.client_id'));
-        $this->setClientSecret(config('infusionsoft.client_secret'));
-        $this->setRedirectUri(url(config('infusionsoft.redirect_uri')));
+        $this->store = config('infusionsoft.cache');
+
+        // Determine the cache token name
+        if ($account) {
+            $this->setTokenName(sprintf('%s.%s', config('infusionsoft.token_name'), $account));
+        } else {
+            $this->setTokenName(sprintf('%s.default', config('infusionsoft.token_name')));
+        }
+
+        if (config('infusionsoft.multi') && $account) {
+            $infusionsoft_accounts = json_decode(trim(stripslashes(config('infusionsoft.accounts'))), true);
+            $infusionsoft_account = current(array_filter($infusionsoft_accounts, function ($infusionsoft_account) use ($account) {
+                return $account === array_key_first($infusionsoft_account);
+            }));
+
+            if (!$infusionsoft_account) {
+                throw new \Infusionsoft\InfusionsoftException(
+                    'Infusionsoft account could not be found in config using multi',
+                    1
+                );
+            }
+
+            $client_id = $infusionsoft_account[$account]['client_id'];
+            $client_secret = $infusionsoft_account[$account]['client_secret'];
+            $redirect_uri = $infusionsoft_account[$account]['redirect_uri'];
+        } elseif (config('infusionsoft.multi')) {
+            // There was a problem with config setup
+            throw new \Infusionsoft\InfusionsoftException(
+                'Infusionsoft is set up for multi, but no account was set',
+                1
+            );
+        } else {
+            $client_id = config('infusionsoft.client_id');
+            $client_secret = config('infusionsoft.client_secret');
+            $redirect_uri = config('infusionsoft.redirect_uri');
+        }
+
+        // Check for existing client id
+        if (empty($client_id)) {
+            throw new \Infusionsoft\InfusionsoftException('Infusionsoft Client ID not present', 1);
+        }
+        // Check for existing client secret
+        if (empty($client_secret)) {
+            throw new \Infusionsoft\InfusionsoftException('Infusionsoft Client Secret not present', 1);
+        }
+        // Check for existing infusionsoft.token
+        if (cache()->store(config('infusionsoft.cache'))->has(config('infusionsoft.token_name'))) {
+            throw new \Infusionsoft\InfusionsoftException(
+                'Infusionsoft token file exists, please remove it to reauthorize with Infusionsoft'
+            );
+        }
+
+        $this->setClientId($client_id);
+        $this->setClientSecret($client_secret);
+        $this->setRedirectUri(url($redirect_uri));
+
         if (config('infusionsoft.debug')) {
             $this->setDebug(true);
         }
-        $new_token = false;
-        $store = config('infusionsoft.cache');
-        if (cache()->store($store)->has($this->token_name)) {
-            $token = unserialize(cache()->store($store)->get($this->token_name));
-            $this->setToken(new Token($token));
+        if (cache()->store($this->store)->has($this->token_name)) {
+            // If we aleady have a token, use it
+            $token = unserialize(cache()->store($this->store)->get($this->token_name));
+            $this->setToken(new \Infusionsoft\Token($token));
         } elseif (request()->has('code')) {
+            // Request a new token if we have a code
             $this->requestAccessToken(request()->get('code'));
-            $new_token = true;
+            $this->storeAccessToken();
         } else {
-            throw new InfusionsoftException(sprintf(
-                'You must authorize your application here: %s',
-                $this->getAuthorizationUrl()
-            ), 1);
-        }
-        $token = $this->getToken();
-        $expired = ($token->getEndOfLife() - (time() * 2)) <= 0 ? true : false;
-        if ($expired || $new_token) {
-            $extra = $token->getExtraInfo();
-            if (!$new_token) {
-                $token = $this->refreshAccessToken();
-            }
-            cache()->store($store)->forever($this->token_name, serialize([
-                "access_token" => $token->getAccessToken(),
-                "refresh_token" => $token->getRefreshToken(),
-                "expires_in" => $token->getEndOfLife(),
-                "token_type" => $extra['token_type'],
-                "scope" => $extra['scope'],
-            ]));
+            // Authorize the app to use the API
+            return redirect($this->getAuthorizationUrl());
+            // throw new InfusionsoftException(sprintf(
+            //     'You must authorize your application here: %s',
+            //     $this->getAuthorizationUrl()
+            // ));
         }
     }
 
@@ -95,5 +137,26 @@ class Infusionsoft extends Inf
     {
         $this->token_name = $name;
         return $this;
+    }
+
+    public function storeAccessToken()
+    {
+        $token = $this->getToken();
+        $extra = $token->getExtraInfo();
+
+        // Determine if expiring
+        $expired = ($token->getEndOfLife() - (time() * 2)) <= 0;
+        if ($expired) {
+            // Request new access token with refresh token
+            $token = $this->refreshAccessToken();
+        }
+
+        cache()->store($this->store)->forever($this->token_name, serialize([
+            'access_token' => $token->getAccessToken(),
+            'refresh_token' => $token->getRefreshToken(),
+            'expires_in' => $token->getEndOfLife(),
+            'token_type' => $extra['token_type'],
+            'scope' => $extra['scope'],
+        ]));
     }
 }
